d0b7bd9c10829d0c57b98f7ca6bd919fcc30ecbd
Koushikdev15 <koushikiam18@gmail.com>
2026-08-07 10:03:24 +0530

    update project files

--- herbchain_backend/src/routes/publicRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { PublicController } from '../controllers/PublicController';
-
-const router = Router();
-const controller = new PublicController();
-
-router.get('/provenance/:batchId', controller.getProvenance);
-
-export default router;
